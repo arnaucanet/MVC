c1b073424b937ec65519a5df55810d6bf2a8c1e7
Arnau Canet <arnaucanetfp@ibf.cat>
2025-12-18 17:36:53 +0100

    cambios

--- controller/PedidoController.php
+++ controller/PedidoController.php
@@ -6,7 +6,7 @@ class PedidoController {
         // verificamos que recibimos datos por tramitar pedido
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cart_data'])) {
             
-            // 2. Recogemos los datos del carrito que vienen en formato texto (JSON)
+            //datos del carrito
             $datosEnBruto = $_POST['cart_data'];
             
             //convertir texto en array asociativo y no en objeto
